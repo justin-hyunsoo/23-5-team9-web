e0807077c67de41fe89d7be7519aec1165443d1f
hyunsoo <hyunsoo3318@snu.ac.kr>
2026-01-08 12:25:57 +0900

    Fix Router basename to match team repository path

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './styles/common.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router basename="/dangeun_toy_front">
+    <Router basename="/23-5-team9-web">
       <App />
     </Router>
   </StrictMode>
